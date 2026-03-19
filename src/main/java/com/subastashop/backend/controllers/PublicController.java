82e90d5d6f4010063fdded7b8a3e83e260a98f02
crisber <bcbs1986@gmail.com>
2026-03-19 10:33:53 -0300

    mejoras en mensajes y carrito V8

--- src/main/java/com/subastashop/backend/controllers/PublicController.java
+++ src/main/java/com/subastashop/backend/controllers/PublicController.java
@@ -10,13 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable; // Importar PathVariable
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/public")
-@CrossOrigin(origins = "*")
 public class PublicController {
 
     @Autowired
