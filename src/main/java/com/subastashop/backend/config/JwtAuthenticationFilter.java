f53492f20548b85eca4b3b80edcd0f205c25f336
crisber <bcbs1986@gmail.com>
2026-04-16 20:04:36 -0400

    nuevas mejoras

--- src/main/java/com/subastashop/backend/config/JwtAuthenticationFilter.java
+++ src/main/java/com/subastashop/backend/config/JwtAuthenticationFilter.java
@@ -27,6 +27,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
         String jwtCandidate = null;
 
         // 1. Intentar obtener de Header
